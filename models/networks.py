90f151b4fc3100967d7c11a863a9e204de385724
amy-hyunji <alee6868@kaist.ac.kr>
2020-01-17 10:32:17 +0900

    before debugging
    need debugging

--- models/networks.py
+++ models/networks.py
@@ -24,6 +24,17 @@ def deconv(in_channels, out_channels, activation = "relu"):
 				nn.ReLU()
 				)
 
+def upconv(in_channels, out_channels, mode="transpose"):
+	if mode == "transpose":
+		return nn.ConvTranspose2d(
+			in_channels, out_channels, kernel_size=2, stride=2
+			)
+	else:
+		return nn.Sequential(
+			nn.Upsample(mode="bilinear", scale_factor=2),
+			nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=True, groups=1)
+			)
+
 def predict_flow(in_channels):
 	return nn.Conv2d(in_channels, 2, kernel_size=3, stride=1, padding=1, bias=True)
 
@@ -67,32 +78,38 @@ TODO: change channels to be dependent on input channels
 """
 class STN(nn.Module):
 	# NEED to check channel number ==> output should be 3*@
-	def __init__(self, input_ch, h, w):
+	def __init__(self, input_ch):
 		super(STN, self).__init__()
 		# localization network
 		self.localization = nn.Sequential(
 			conv(input_ch, 8, 1, kernel_size=7),
 			nn.MaxPool2d(2, stride=2),
 			nn.ReLU(True),
-			conv(8, 10, kernel_size=5),
+			conv(8, 10, 1, kernel_size=5),
 			nn.MaxPool2d(2, stride=2),
 			nn.ReLU(True)
 		)
 
 		# Regressor for the 3*2 affine matrix
-		self.fc_loc = nn.Sequential(
-			nn.Linear(10*3*3, 32),
+		self.fc_loc = NULL 
+
+	def fc_loc(self, input):
+		return nn.Sequential(
+			nn.Linear(input, 32),
 			nn.ReLU(True),
 			nn.Linear(32, 3*2)
-		)
+			)
 
+	def forward(self, x):
+		x = self.localization(x.view(28, 28))) #resize x to (28, 28)
+		x_shape = x.shape
+		x = x.view(-1, 10 * x_shape[-1] * x_shape[-2])
+
+		self.fc_loc = fc_loc(10 * x_shape[-1] * x_shape[-2]) # 가능..?
 		#Initialize the weights/bias with identity transformation
 		self.fc_loc[2].weight.data.zero_()
 		self.fc_loc[2].bias.data.copy_(torch.tensor[1, 0, 0, 0, 1, 0], dtype=torch.float)
 
-	def forward(self, x):
-		x = self.localization(x.view(28, 28))) #resize x to (28, 28)
-		x = x.view(-1, 10*3*3)
 		theta = self.fc_loc(x)
 		theta = theta.view(-1, 2, 3) # matrix for transformation
 
@@ -104,24 +121,28 @@ class STN(nn.Module):
 class for Feature Pyramid Networks
 """
 class FTN(nn.Module):
-	def __init__(self, N, input_ch):
+	def __init__(self, N, ch_list):
 		super(FTN, self).__init__()
 		self.N = N
 		self.ret_list = []
+		self.ch = ch_list
 
-		# encoding layer
+		# encoding layer - left to right
 		self.conv = []
 		for i in range(N):
-			self.conv.append(BasicBlock(input_ch * (i+1))) 
+			self.conv.append(BasicBlock(self.ch[i])) 
 
-		#decoding layer
+		# decoding layer - left to right
 		self.deconv = []
 		for i in range(N-1):
-			self.deconv.append(deconv(input_ch * (N - i), input_ch * (N - i - 1)))
-		
-		self.upsample = nn.Upsampling(scale_factor=2, mode="nearest")
+			self.deconv.append(deconv(self.ch[-1-i], self.ch[-2-i]))
+	 
+		# upsampling layer - right to left
+		self.upsample = []
+		for i in range(N-1):
+			self.upsample.append(upsample(self.ch[N-i], self.ch[N-1-i]))
 
-	 def forward(self, input):
+	def forward(self, input):
 		ret = input
 		for i in self.N:
 			ret = self.conv[i](ret)
@@ -129,50 +150,60 @@ class FTN(nn.Module):
 		return ret
 
 	"""
-	TODO: need to change concat to add
+	TODO: need to change concat to add - ok
 	"""
 	 def deconv_forward(self, input, i):
-		_deconv = self.deconv[i].forward(input)
-		_conv = torch.cat(self.upsample(self.ret_list[0]), self.ret_list[1])
-		return torch.cat(_deconv, _conv)
+		ret_dec = self.deconv[i](input)
+		upsample = self.upsample[i](self.ret_list[self.N - i])
+		add_enc = _upsample.add(self.ret_list[N-i-1])
+		return torch.cat([ret_dec, add_ret], 1)
 	
-
+"""
+TODO: max channel should be 256 - ok
+"""
 class FlowNet(nn.Module):
-	def __init__(self, N, input_ch):
+	def __init__(self, N, input_ch, h, w):
 		self.N = N
 
-		self.SourceFTN = FTN(N, input_ch)
-		self.TargetFTN = FTN(N, input_ch)
+		# define channel list
+		self.ch = []
+		for i in range(N):
+			self.ch.append(min(input_ch*(i+1), 256))
 
-		# E layer
+		self.SourceFTN = FTN(N, self.ch)
+		self.TargetFTN = FTN(N, self.ch)
+
+		# list for Warp - left to right
+		self.stn = []
+		for i in range(N-1):
+			self.stn.append(STN(self.ch[-2-i]))
+
+		# E layer - left to right
 		self.E = []
 		for i in range(N):
-			self.E.append(deconv(input_ch*(N - i)*2, input_ch*(N - i -1)*2))
+			# multiple by 2 due to concat (Sn, Tn)
+			self.E.append(predict_flow(self.ch[-1-i] * 2))
 
 		self.upsample = nn.Upsample(scale_factor=2, mode="nearest")
 
 	def forward(self, src, tar):
-		"""
-		predict warping image by putting into predict_flow
-		"""
-
 		#input source,target image
-		src_conv = self.SourceFTN(src)
-      tar_conv = self.TargetFTN(tar)
+		src_conv = self.SourceFTN(src) #[W, H, C]
+      tar_conv = self.TargetFTN(tar) #[W, H, C]
 
 		"""
 		TODO: change code
-				E => predict_flow 사용
+				E => predict_flow 사용 - ok
 		"""
 		#concat for E4 to E1
-      for i in range(self.N):
-			if (i==0):
-				F = self.E[i](torch.cat((src_conv, tar_conv), 1))
-			else:
-				F = self.upsample(F).add(self.E[i+1](concat))
-			src_conv = self.SourceFTN.deconv_forward(src_conv, i)
-			tar_conv = self.TargetFTN.deconv_forward(tar_conv, i)
-			warp = STN(torch.cat(src_conv, self.upsample(F))) # concat?
-			concat = torch.cat(warp, tar_conv), 1)
+		F = self.E[0](torch.cat([src_conv, tar_conv], 1)) #[W, H, 2]
+      for i in range(self.N - 1):
+			src_conv = self.SourceFTN.deconv_forward(src_conv, i) #[2W, 2H, C]
+			tar_conv = self.TargetFTN.deconv_forward(tar_conv, i) #[2W, 2H, C]
+			upsample_F = self.upsample(F)
+			warp = self.stn[i](torch.cat([src_conv, upsample_F], 1)) #concat?
+			concat = torch.cat([warp, tar_conv], 1)
+			F = upsample_F.add(self.E[i+1](concat))
+		return F
 		
 
