6e76403ca84cdc8791965184db00c86dcc932e81
Taeho's MacBook Pro <>
2020-01-17 15:20:31 +0900

    modify train.py

--- train.py
+++ train.py
@@ -7,21 +7,36 @@ import torchvision
 from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 import numpy as np
+from models.networks import *
+from dataloader import *
 
-optimizer = optim.SGD(model.parameters(), lr=0.01)
+INPUT_SIZE = (1024, 1024)
+EPOCHS = 200
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def train(epoch):
+    model = FlowNet(4, 3, INPUT_SIZE[0], INPUT_SIZE[1]).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0002, betas=(0.5, 0.999))
+    train_loader = ClothDataLoader(opt, train_dataset)
+
+    model.cuda()
     model.train()
+
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
 
         optimizer.zero_grad()
-        output = model(data)
-        loss = F.nll_loss(output, target)
-        loss.backward()
+        model(data)
+        model.backward()
         optimizer.step()
-        if batch_idx % 500 == 0:
+        result = model.current_results()
+
+        if batch_idx % 50 == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()))
+                100. * batch_idx / len(train_loader), result['loss']))
+
+if __name__ == '__main__':
+    for i in range(EPOCHS):
+        train(i)
